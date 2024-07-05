4fd31da27e44ab39bb75860705fbbe203c4e23f5
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-05 10:01:11 -0600

    feat: save the arg values in var

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    dbg!(&args);
+    let query = &args[1];
+    let file_name = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_name}");
+
+    // dbg!(&args);
 }
