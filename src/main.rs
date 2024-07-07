02f0291c5d38f2e1a0acef38d3478cd62a0d04d2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-07 10:01:11 -0600

    feat: read from test file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,6 +8,10 @@ fn main() {
 
     println!("Searching for {query}");
     println!("In file {file_name}");
+
+    let contents = fs::read_to_string(file_name).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 
     // dbg!(&args);
 }
