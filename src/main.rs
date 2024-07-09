4abf3bcb5dcfbee9296297499f8bf2f09aae164b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-09 10:01:11 -0600

    feat: use struct

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,29 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_name) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_name}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_name).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 
     // dbg!(&args);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {
+        query,
+        file_path
+    }
 }
