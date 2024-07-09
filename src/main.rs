f507df51c29993598113b83e633f8d7af1f9d790
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-09 10:01:11 -0600

    refactor: create a parse function

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_name = &args[2];
+    let (query, file_name) = parse_config(&args);
 
     println!("Searching for {query}");
     println!("In file {file_name}");
@@ -14,4 +13,11 @@ fn main() {
     println!("With text:\n{contents}");
 
     // dbg!(&args);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
