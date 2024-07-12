0749dfabfb117805071a0be981a639cfbda5113e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-12 10:01:11 -0600

    refactor: fix a warning message

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
-        if (args.len() < 3) {
+        if args.len() < 3 {
             panic!("not enough arguments!");
         }
 
