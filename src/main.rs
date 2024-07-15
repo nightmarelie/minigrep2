f7b900635fe5f10fa6cd2bf59dddabb86180a4c0
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-15 10:01:15 -0600

    refactor: split by lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::{env, process};
+use minigrep2::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,29 +20,4 @@ fn main() {
     // dbg!(&args);
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
