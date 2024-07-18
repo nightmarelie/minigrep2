174ad015a3e37674526ae446658a81235d0503ea
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-18 10:01:15 -0600

    refactor: make a test pass

--- src/lib.rs
+++ src/lib.rs
@@ -26,11 +26,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    for line in contents.lines() {
+    let mut results = Vec::new();
 
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
     }
 
-    vec![]
+    results
 }
 
 #[cfg(test)]
@@ -41,8 +45,8 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-Rust:\
-safe, fast, productive.\
+Rust:
+safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
