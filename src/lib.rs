695297d924f1d178b344e9916a51a4b2927769d7
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-20 10:01:15 -0600

    refactor: add case sensitive and case insensitive tests

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,30 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me!";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me!";
+
+        assert_eq!(vec!["Rust:", "Trust me!"], search(query, contents));
     }
 }
